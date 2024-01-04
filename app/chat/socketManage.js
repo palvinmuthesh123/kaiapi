69145dbf96f5555731736a676c926aac1d2900dd
Palvin Muthesh <spalvinmuthesh@gmail.com>
2024-01-04 10:37:30 +0530

    jkjkjk

--- app/chat/socketManage.js
+++ app/chat/socketManage.js
@@ -26,11 +26,12 @@ module.exports = io => socket => {
         // ];
 
         var registrationTokens = await User.find({_id: msg.to}).sort({createdAt: 1}).lean()
+        var registrationTokens1 = await User.find({_id: msg.from}).sort({createdAt: 1}).lean()
 
         var payload = {
             notification: {
-                title: 'Testing',
-                body : 'TEST'
+                title: registrationTokens1[0].first_name + " " + registrationTokens1[0].last_name + " sent you a message",
+                body : msg.message
             }
         };
         
