32e9711ef15b32f2c6e16d3c4bddb6f9c953d1b7
Palvin Muthesh <spalvinmuthesh@gmail.com>
2024-01-04 10:25:29 +0530

    jklhgfkhdfglghghf

--- app/chat/socketManage.js
+++ app/chat/socketManage.js
@@ -1,7 +1,9 @@
 const db = require('_helpers/db');
 const Chat = db.Chat;
+const User = db.User;
 const DoctorChat = db.DoctorChat;
-// const {admin} = require("../../firebase-config")
+const {admin} = require("../../firebase-config")
+
 
 module.exports = io => socket => {
     console.log("Made socket connection", socket.id, );
@@ -18,6 +20,34 @@ module.exports = io => socket => {
             const message = new DoctorChat(msg);
             await message.save();
         }
+
+        // var registrationTokens = [
+        //     'tokenFromIosApp'
+        // ];
+
+        var registrationTokens = await User.find({_id: msg[i].to}).sort({createdAt: 1}).lean()
+
+        var payload = {
+            notification: {
+                title: 'Testing',
+                body : 'TEST'
+            }
+        };
+        
+        admin.messaging().sendToDevice(registrationTokens.deviceid, payload)
+            .then((response) => {
+                console.log('Sent successfully.\n');
+                console.log(response);
+                res.status(statusCodes.Ok);
+                res.json(response);
+            })
+            .catch((error) => {
+                console.log('Sent failed.\n');
+                console.log(error);
+                res.status(statusCodes.InternalServerError);
+                res.json(error);
+            });
+
     });
 
     socket.on("disconnect", () => {
