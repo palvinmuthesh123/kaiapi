b400bce4558a3155cfab474b9693850802007150
Palvin Muthesh <spalvinmuthesh@gmail.com>
2024-01-04 09:59:30 +0530

    iohefioheiofhiefie

--- app/chat/socketManage.js
+++ app/chat/socketManage.js
@@ -1,5 +1,6 @@
 const db = require('_helpers/db');
 const Chat = db.Chat;
+const DoctorChat = db.DoctorChat;
 // const {admin} = require("../../firebase-config")
 
 module.exports = io => socket => {
@@ -7,8 +8,16 @@ module.exports = io => socket => {
     socket.on("message", async(msg) => {
         console.log(msg);
         io.emit("message", msg);
-        const message = new Chat(msg);
-        await message.save();
+        if(msg.who=='jobs')
+        {
+            const message = new Chat(msg);
+            await message.save();
+        }
+        else if(msg.who=='doctor')
+        {
+            const message = new DoctorChat(msg);
+            await message.save();
+        }
     });
 
     socket.on("disconnect", () => {
